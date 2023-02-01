b09336aad45cf5764a7e4546b84ffe75cb7fe9d5
HeikTomi <heikkinentomi@hotmail.com>
2023-02-01 19:16:02 +0200

    Fixed admin ID

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ db.create_all()
 def admin_only(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if current_user.id != 4:
+        if current_user.id != 1:
             return abort(403)
         return f(*args, **kwargs)
 
